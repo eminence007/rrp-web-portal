bb1122468a03ccb0be859afd738a47ea5c78e0e7
AmitKSwain <amit.here1@gmail.com>
2021-10-05 16:20:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div></div>
+    <div>HI</div>
   );
 }
+export default App
