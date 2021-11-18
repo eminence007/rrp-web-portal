f937ae8db6926b9b15be3a27442e338669d47a90
AmitKSwain <amit.here1@gmail.com>
2021-11-18 16:17:20 +0530

    1 file modified

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Intro.css'
 function Intro(){
   return (<p className = 'intro-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id officia tempore debitis ut mollitia dolorum quisquam quam ab? Cumque nihil soluta laboriosam perspiciatis sint assumenda quam animi eos aperiam ipsam, impedit rerum mollitia consequuntur asperiores beatae debitis inventore excepturi provident id est recusandae distinctio perferendis ducimus. Reiciendis, mollitia? Perferendis, distinctio. </p>)
 }
