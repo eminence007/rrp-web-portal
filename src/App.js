251ab214996433c8682430368b19589e09baf83b
AmitKSwain <amit.here1@gmail.com>
2021-11-18 12:31:11 +0530

    1 file modified, 4 added, 4 deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Nav from './components/Layouts/Nav'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home/Home'
-import Footer from './components/Layouts/Footer'
+import Footer from './components/layouts/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 
