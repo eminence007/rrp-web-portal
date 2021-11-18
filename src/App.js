d1ef6bf93146a0f97aaa793f1815c8d09a2717a3
AmitKSwain <amit.here1@gmail.com>
2021-11-18 15:19:10 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home'
-import Home from './components/layout/Layout'
+import Layout from './components/layouts/Layout'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 
