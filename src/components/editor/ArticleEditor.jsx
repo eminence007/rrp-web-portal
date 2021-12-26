b700c45e36eaa45abe2cd1c3a1cf245a779e8935
Amit Kumar Swain <amit.here1@gmail.com>
2021-12-26 10:46:56 +0530

    new file creation code added

--- src/components/editor/ArticleEditor.jsx
+++ src/components/editor/ArticleEditor.jsx
@@ -1,19 +1,53 @@
-
-import React from 'react';
-import MarkdownIt from 'markdown-it';
-import MdEditor from 'react-markdown-editor-lite';
-
-import 'react-markdown-editor-lite/lib/index.css';
-
+import React, { useState, useCallback, useEffect } from "react";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
 const mdParser = new MarkdownIt();
+let firstLoad = true;
+const ArticleEditor = (props) => {
+  const [articleText, setArticleText] = useState("");
+  function handleEditorChange({ html, text }) {
+    //console.log("handleEditorChange", html, text);
+    setArticleText(text);
+  }
+  const handleSubmission = useCallback(async () => {
+    console.log("The text is :" + articleText);
+    var articlefile = new File(articleText, "article1.md", {
+      type: "text/markdown"
+    });
+    const formData = new FormData();
+    formData.append("profilePhoto", articlefile);
+    const response = await fetch(
+      "https://rrp-web-api.herokuapp.com/user/photoUpload",
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }, [articleText]);
 
-// Finish!
-function handleEditorChange({ html, text }) {
-  console.log('handleEditorChange', html, text);
-}
- const ArticleEditor=(props) => {
+  useEffect(() => {
+    if (firstLoad) {
+      firstLoad = false;
+      return;
+    }
+    handleSubmission();
+  }, [handleSubmission]);
+
   return (
-    <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+    <>
+      <button onClick={handleSubmission}>Submit</button>
+      <MdEditor
+        style={{ height: "500px" }}
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={handleEditorChange}
+      />
+    </>
   );
 };
-export default ArticleEditor
+export default ArticleEditor;
