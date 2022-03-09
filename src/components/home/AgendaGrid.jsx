0b28efc490a101828f1429c60565a9f017d08770
AmitKSwain <amit.here1@gmail.com>
2022-03-09 21:29:58 +0530

    hindi text added

--- src/components/home/AgendaGrid.jsx
+++ src/components/home/AgendaGrid.jsx
@@ -4,43 +4,43 @@ import classes from './AgendaGrid.module.css'
 function AgendaGrid(){
   return (
  <>
- <h1 className={classes['agendatitle']}>Key Agendas</h1>
+ <h1 className={classes['agendatitle']}>मुख्य मुद्दे</h1>
   
  <Container ><Row>
     <Col xs={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']} >
     <Card.Img className={classes['image']} variant="bottom" src="https://i.postimg.cc/ZKtrSnB7/gold.png" />
-    <Card.Body><Card.Title>Dhan wapsi</Card.Title></Card.Body>
+    <Card.Body><Card.Title>धनवापसी पासबुक </Card.Title></Card.Body>
     </Card>
     </Col>
     <Col xs={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']}>
     <Card.Img className={classes['image']} variant="bottom" src="https://i.postimg.cc/vTqKjnXm/jury2.png" />
-    <Card.Body><Card.Title>Jury Court</Card.Title></Card.Body>
+    <Card.Body><Card.Title>जूरी कोर्ट</Card.Title></Card.Body>
    </Card>
     </Col>
     <Col sm={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']}>
     <Card.Img className={classes['image']} variant="bottom" src="https://cdn-icons-png.flaticon.com/128/6140/6140261.png" />
-    <Card.Body><Card.Title>Empty Land Tax</Card.Title></Card.Body>
+    <Card.Body><Card.Title>रिक्त भूमि कर</Card.Title></Card.Body>
     </Card>
     </Col>
     <Col sm={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']}>
     <Card.Img className={classes['image']} variant="bottom" src="https://cdn-icons-png.flaticon.com/128/1419/1419216.png" />
-    <Card.Body><Card.Title>Hindu Board</Card.Title></Card.Body>
+    <Card.Body><Card.Title>हिन्दूबोर्ड </Card.Title></Card.Body>
     </Card>
     </Col>
     <Col sm={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']}>
     <Card.Img className={classes['image']} variant="bottom" src="https://cdn-icons-png.flaticon.com/128/556/556206.png" />
-    <Card.Body><Card.Title>Gun Law</Card.Title></Card.Body>
+    <Card.Body><Card.Title>गन लॉ</Card.Title></Card.Body>
     </Card>
     </Col>
     <Col sm={12} md={6} lg={4} className={classes['row-gap']}>
     <Card className={classes['card']}>
     <Card.Img className={classes['image']} variant="bottom" src="https://i.postimg.cc/vZgDNC5d/vvcm.png" />
-    <Card.Body><Card.Title>Votevapsi CM</Card.Title></Card.Body>
+    <Card.Body><Card.Title>वोटवापसी मुख्यमंत्री </Card.Title></Card.Body>
     </Card>
     </Col>
   </Row>
