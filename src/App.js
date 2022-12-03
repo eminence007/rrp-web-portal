d4f8bce87e7c2a8fba14a3585d1447667fa2c976
AmitKSwain <amit.here1@gmail.com>
2022-12-03 16:34:17 +0530

    using hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import { HashRouter as Router, Route,Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './components/layouts/Layout';
 import Home from './components/pages/Home'
